7ca6b1fc34f3dbba4f5f20ad18c55be3315e88c7
= <=>
2023-09-11 08:51:47 +0200

    .

--- webpagina/javascript/rekenhulp.js
+++ webpagina/javascript/rekenhulp.js
@@ -8,9 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const generateButton = document.getElementById("generateButton");
     const showAnswerButton = document.getElementById("showAnswerButton");
     const operationSelect = document.getElementById("operationSelect");
+    const operatorSymbol = document.getElementById("operatorSymbol");
 
     let randomNum1 = 0;
     let randomNum2 = 0;
+    let selectedOperation = operationSelect.value; // Initialize selected operation
 
     function generateRandomNumbers() {
         randomNum1 = Math.floor(Math.random() * 13);
@@ -19,19 +21,28 @@ document.addEventListener("DOMContentLoaded", function () {
         number1.textContent = randomNum1;
         number2.textContent = randomNum2;
         number3.textContent = ""; // Clear square 3 when generating new numbers
+        
+        // Set the operator symbol based on the selected operation
+        if (selectedOperation === "multiplication") {
+            operatorSymbol.textContent = "X";
+        } else if (selectedOperation === "division") {
+            operatorSymbol.textContent = ":";
+        } else if (selectedOperation === "mix") {
+            // Randomly choose multiplication or division
+            operatorSymbol.textContent = Math.random() < 0.5 ? "X" : ":";
+        }
     }
 
     generateButton.addEventListener("click", generateRandomNumbers);
 
     showAnswerButton.addEventListener("click", function () {
-        const selectedOperation = operationSelect.value;
         let result = 0;
 
         if (selectedOperation === "multiplication") {
             result = randomNum1 * randomNum2;
         } else if (selectedOperation === "division") {
             if (randomNum2 === 0) {
-                result = "niet mogelijk";
+                result = "Division by zero!";
             } else {
                 // Ensure number1 is greater than or equal to number2
                 if (randomNum1 < randomNum2) {
@@ -53,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 result = randomNum1 * randomNum2;
             } else {
                 if (randomNum2 === 0) {
-                    result = "niet mogelijk";
+                    result = "Division by zero!";
                 } else {
                     // Ensure number1 is greater than or equal to number2
                     if (randomNum1 < randomNum2) {
@@ -71,6 +82,21 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         number3.textContent = result;
+    });
+
+    // Update the selected operation when it changes
+    operationSelect.addEventListener("change", function () {
+        selectedOperation = operationSelect.value;
+        
+        // Set the operator symbol based on the selected operation
+        if (selectedOperation === "multiplication") {
+            operatorSymbol.textContent = "X";
+        } else if (selectedOperation === "division") {
+            operatorSymbol.textContent = ":";
+        } else if (selectedOperation === "mix") {
+            // Randomly choose multiplication or division
+            operatorSymbol.textContent = Math.random() < 0.5 ? "X" : ":";
+        }
     });
 
     // Generate initial random numbers

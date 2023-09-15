1bb99fae4077de901d3e6d42e2c60db1393bf202
Freddy Meijer <freddymeijer@hotmail.com>
2023-09-15 19:29:53 +0200

    Er is nu mogelijkheid om de tafels te kiezen.

--- javascript/rekenhulp.js
+++ javascript/rekenhulp.js
@@ -9,14 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const showAnswerButton = document.getElementById("showAnswerButton");
     const operationSelect = document.getElementById("operationSelect");
     const operatorSymbol = document.getElementById("operatorSymbol");
+    const tafelsInput = document.getElementById("tafels")
 
     let randomNum1 = 0;
     let randomNum2 = 0;
-    let selectedOperation = operationSelect.value; // Initialize selected operation
+    let selectedOperation = operationSelect.value;
 
     function generateRandomNumbers() {
-        randomNum1 = Math.floor(Math.random() * 13);
-        randomNum2 = Math.floor(Math.random() * 13);
+        const maxTafels = parseInt(tafelsInput.value);
+        randomNum1 = Math.floor(Math.random() * (maxTafels + 1));
+        randomNum2 = Math.floor(Math.random() * (maxTafels + 1)); 
 
         number1.textContent = randomNum1;
         number2.textContent = randomNum2;
@@ -44,33 +46,28 @@ document.addEventListener("DOMContentLoaded", function () {
             if (randomNum2 === 0) {
                 result = randomNum1;
             } else {
-
                 const quotient = Math.floor(randomNum1 / randomNum2);
                 const remainder = randomNum1 % randomNum2;
                 if (quotient === 0) {
                     result = "kan niet";
-                } else if (remainder === 0 ) {
+                } else if (remainder === 0) {
                     result = quotient;
                 } else {
                     result = `${quotient} rest ${remainder}`;
                 }
             }
         } else if (selectedOperation === "mix") {
-            // Randomly choose multiplication or division
-
             if (operatorSymbol.textContent === "X") {
                 result = randomNum1 * randomNum2;
             } else {
                 if (randomNum2 === 0) {
                     result = randomNum1;
                 } else {
-
                     const quotient = Math.floor(randomNum1 / randomNum2);
                     const remainder = randomNum1 % randomNum2;
-
                     if (quotient === 0) {
                         result = "kan niet";
-                    } else if (remainder === 0 ) {
+                    } else if (remainder === 0) {
                         result = quotient;
                     } else {
                         result = `${quotient} rest ${remainder}`;
@@ -78,26 +75,25 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             }
         }
-    number3.textContent = result;
-});
 
-// Update the selected operation when it changes
-operationSelect.addEventListener("change", function () {
-    selectedOperation = operationSelect.value;
+        number3.textContent = result;
+    });
 
-    // Set the operator symbol based on the selected operation
-    if (selectedOperation === "multiplication") {
-        operatorSymbol.textContent = "X";
-    } else if (selectedOperation === "division") {
-        operatorSymbol.textContent = ":";
-    } else if (selectedOperation === "mix") {
-        // Randomly choose multiplication or division
-        operatorSymbol.textContent = Math.random() < 0.5 ? "X" : ":";
-    }
-});
+    // Update the selected operation when it changes
+    operationSelect.addEventListener("change", function () {
+        selectedOperation = operationSelect.value;
 
-// Generate initial random numbers
-generateRandomNumbers();
+        if (selectedOperation === "multiplication") {
+            operatorSymbol.textContent = "X";
+        } else if (selectedOperation === "division") {
+            operatorSymbol.textContent = ":";
+        } else if (selectedOperation === "mix") {
+            operatorSymbol.textContent = Math.random() < 0.5 ? "X" : ":";
+        }
+    });
+
+    // Generate initial random numbers
+    generateRandomNumbers();
 });
 
 // JavaScript code in your rekenhulp.js file
@@ -105,25 +101,25 @@ const dashboard = document.querySelector('.dashboard');
 let offsetX, offsetY, isDragging = false;
 
 dashboard.addEventListener('mousedown', (e) => {
-  isDragging = true;
-  offsetX = e.clientX - dashboard.getBoundingClientRect().left;
-  offsetY = e.clientY - dashboard.getBoundingClientRect().top;
-  dashboard.style.cursor = 'grabbing';
-  dashboard.style.userSelect = 'none';
+    isDragging = true;
+    offsetX = e.clientX - dashboard.getBoundingClientRect().left;
+    offsetY = e.clientY - dashboard.getBoundingClientRect().top;
+    dashboard.style.cursor = 'grabbing';
+    dashboard.style.userSelect = 'none';
 });
 
 document.addEventListener('mousemove', (e) => {
-  if (!isDragging) return;
+    if (!isDragging) return;
 
-  const newX = e.clientX - offsetX;
-  const newY = e.clientY - offsetY;
+    const newX = e.clientX - offsetX;
+    const newY = e.clientY - offsetY;
 
-  dashboard.style.left = `${newX}px`;
-  dashboard.style.top = `${newY}px`;
+    dashboard.style.left = `${newX}px`;
+    dashboard.style.top = `${newY}px`;
 });
 
 document.addEventListener('mouseup', () => {
-  isDragging = false;
-  dashboard.style.cursor = 'grab';
-  dashboard.style.userSelect = 'auto';
+    isDragging = false;
+    dashboard.style.cursor = 'grab';
+    dashboard.style.userSelect = 'auto';
 });
